142f86598eb80ab3b2d7fd004971f7bf58db9df5
shanto173 <kamrul0628@gmail.com>
2025-06-24 13:21:25 +0600

    Fix invoice file

--- downLoad_Invoice_summary.py
+++ downLoad_Invoice_summary.py
@@ -30,11 +30,11 @@ download_dir = os.path.join(os.getcwd(), "download")
 os.makedirs(download_dir, exist_ok=True)
 
 chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument("--headless")  # 🔹 Run Chrome in headless mode
-chrome_options.add_argument("--disable-gpu")  # Optional: disable GPU usage
-chrome_options.add_argument("--window-size=1920,1080")  # Optional: set window size for full rendering
-chrome_options.add_argument("--no-sandbox")  # Optional: for Linux environments
-chrome_options.add_argument("--disable-dev-shm-usage")  # Optional: prevents crashes on some systems
+# chrome_options.add_argument("--headless")  # 🔹 Run Chrome in headless mode
+# chrome_options.add_argument("--disable-gpu")  # Optional: disable GPU usage
+# chrome_options.add_argument("--window-size=1920,1080")  # Optional: set window size for full rendering
+# chrome_options.add_argument("--no-sandbox")  # Optional: for Linux environments
+# chrome_options.add_argument("--disable-dev-shm-usage")  # Optional: prevents crashes on some systems
 chrome_options.add_experimental_option("prefs", {
     "download.default_directory": download_dir,
     "download.prompt_for_download": False,
@@ -91,7 +91,7 @@ while True:
         log.info("=== Trigger global search box by sending a keystroke ===")
         
         body = driver.find_element(By.TAG_NAME, "body")
-        body.send_keys("MRP")  # or use Keys.A if needed
+        body.send_keys("MRP REPORTS")  # or use Keys.A if needed
         time.sleep(2)  # Wait for search box to appear
         
         # Step 5
