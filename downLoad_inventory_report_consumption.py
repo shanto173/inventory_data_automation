2c219a68faccfe15eb00e9e751789a676ae25370
shanto173 <kamrul0628@gmail.com>
2025-07-20 11:01:39 +0600

    Adding consumption file from apr-24 to current data

--- downLoad_inventory_report_consumption.py
+++ downLoad_inventory_report_consumption.py
@@ -29,11 +29,11 @@ download_dir = os.path.join(os.getcwd(), "download")
 os.makedirs(download_dir, exist_ok=True)
 
 chrome_options = webdriver.ChromeOptions()
-# chrome_options.add_argument("--headless")  # Comment this line for debug
-# chrome_options.add_argument("--disable-gpu")
-# chrome_options.add_argument("--window-size=1920,1080")
-# chrome_options.add_argument("--no-sandbox")
-# chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--headless")  # Comment this line for debug
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--window-size=1920,1080")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_experimental_option("prefs", {
     "download.default_directory": download_dir,
     "download.prompt_for_download": False,
