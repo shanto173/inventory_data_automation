8add15d893cf31b2c7fc3da75cc204ecd0f4ddf4
shanto173 <kamrul0628@gmail.com>
2025-07-20 11:13:43 +0600

    Adding consumption file from apr-24 to current data

--- downLoad_inventory_report_consumption.py
+++ downLoad_inventory_report_consumption.py
@@ -173,7 +173,7 @@ try:
         print("Data pasted to Google Sheet (Sheet4).")
         local_tz = pytz.timezone('Asia/Dhaka')
         local_time = datetime.now(local_tz).strftime("%Y-%m-%d %H:%M:%S")
-        worksheet.update("X2", [[f"{local_time}"]])
+        worksheet.update("AA2", [[f"{local_time}"]])
         log.info(f"✅ Data pasted & timestamp updated: {local_time}")
 
     
